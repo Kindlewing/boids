fe275070a565b601a6ee0da49ae7c4850e5580e5
Hudson Finn <hudson.finn@zohomail.com>
2026-01-31 14:32:14 -0500

    start on linalg stuff

---
+++ lib/base/include/linalg.h
@@ -0,0 +1,12 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+#include "typedefs.h"
+#include <math.h>
+
+typedef struct {
+	f32 x;
+	f32 y;
+} vector2f32;
+
+#endif
--- lib/base/include/math.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATH_H
-#define MATH_H
-
-#include "typedefs.h"
-#include <xmmintrin.h>
-
-#endif
